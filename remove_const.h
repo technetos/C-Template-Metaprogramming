e492fe96bff8ad25571b44cc8b92e99df3253119
technetos <jogould@umail.iu.edu>
2016-10-15 16:17:53 -0400

    add remove_const

---
+++ remove_const.h
@@ -0,0 +1,13 @@
+// primary template to handle types that are not const qualified, just return the type given :D
+template<typename T>
+  struct remove_const
+{
+  using type = T;
+};
+
+// partial specialization match for const qualified types
+template<typename T>
+  struct remove_const<T const>
+{
+  using type = T;
+};
