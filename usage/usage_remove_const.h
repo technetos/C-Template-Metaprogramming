629b4af1e5d7cedb44efb5666cd6577ee5e8c9f6
technetos <jogould@umail.iu.edu>
2016-10-17 17:52:42 -0400

    removed usage directory

--- usage/usage_remove_const.h
+++
@@ -1,6 +0,0 @@
-#include"../remove_const.h"
-#include<string>
-
-//remove_const<T>::type t;
-
-remove_const<const std::string>::type t("look a non-const string");
--- usage/usage_type_is.h
+++
@@ -1,4 +0,0 @@
-#include"../type_is.h"
-#include<string>
-
-type_is<std::string>::type t("your generic standard, no funny games, dirt simple identity function");
