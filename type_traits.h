f04f8caeab83f8df2ad85ee44e8cbd95996b7218
technetos <jogould@umail.iu.edu>
2016-10-17 16:27:50 -0400

    added comments to IF

--- type_traits.h
+++ type_traits.h
@@ -35,12 +35,18 @@ namespace META
     };
 
   // Primary template for IF
+  //
+  // IF<true, T, F>::type t
+  // t is of type T
   template<bool, typename T, typename>
     struct IF : type_is<T>
     {
     };
 
   // Partial specalization for matching on false
+  //
+  // IF<false, T, F>::type t
+  // t is of type F
   template<typename T, typename F>
     struct IF<false, T, F> : type_is<F>
     {
