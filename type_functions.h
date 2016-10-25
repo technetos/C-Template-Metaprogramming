fe8a6d8aa6ce1b13146ea99db9ab175f6fd82bdc
technetos <jogould@umail.iu.edu>
2016-10-25 12:17:40 -0400

    updated style and style rules

--- type_functions.h
+++ type_functions.h
@@ -33,9 +33,7 @@ namespace meta { namespace type_func {
     struct prepend_tseq;
 
   template<typename T, typename ...Ts>
-    struct prepend_tseq<T, tseq<Ts...>> : type_is<tseq<T, Ts...>>
-    {
-    };
+    struct prepend_tseq<T, tseq<Ts...>> : type_is<tseq<T, Ts...>> {};
 
 // ---------------------------------------------------------------------------
 
@@ -45,15 +43,11 @@ namespace meta { namespace type_func {
   {
     // Primary template for type_at
     template<size_t N, typename T, typename ...Ts>
-      struct type_at : type_at<N-1, Ts...>
-      {
-      };
+      struct type_at : type_at<N-1, Ts...> {};
 
     // Partial specialization matching on N = 0
     template<typename T, typename ...Ts>
-      struct type_at<0, T, Ts...> : type_is<T>
-      {
-      };
+      struct type_at<0, T, Ts...> : type_is<T> {};
   }
 
   // Grab the first parameter in a parameter pack
@@ -74,21 +68,15 @@ namespace meta { namespace type_func {
 
   // Partial specialization matching on an empty parameter pack
   template<typename T>
-    struct is_one_of<T> : ::std::false_type
-    {
-    };
+    struct is_one_of<T> : ::std::false_type {};
 
   // Partial specialization matching on T being the first element in the pack
   template<typename T, typename ...Ts>
-    struct is_one_of<T, T, Ts...> : ::std::true_type
-    {
-    };
+    struct is_one_of<T, T, Ts...> : ::std::true_type {};
 
   // Partial specialization matching on T not being the head of the pack
   template<typename T, typename T0, typename ...Ts>
-    struct is_one_of<T, T0, Ts...> : is_one_of<T, Ts...>
-    {
-    };
+    struct is_one_of<T, T0, Ts...> : is_one_of<T, Ts...> {};
 
 // ---------------------------------------------------------------------------
 
@@ -97,19 +85,13 @@ namespace meta { namespace type_func {
   namespace detail
   {
     template<typename T>
-      struct inner_type : type_is<T>
-      {
-      };
+      struct inner_type : type_is<T> {};
 
     template<template<typename> typename Tt, typename T>
-      struct inner_type<Tt<T>> : type_is<T>
-      {
-      };
+      struct inner_type<Tt<T>> : type_is<T> {};
 
     template<template<typename ...> typename Tt, typename T, typename ...Ts>
-      struct inner_type<Tt<T, Ts...>> : type_is<T>
-      {
-      };
+      struct inner_type<Tt<T, Ts...>> : type_is<T> {};
   }
 
   template<typename ...Ts>
@@ -125,25 +107,19 @@ namespace meta { namespace type_func {
     // rebind<int, std::string>::type t;
     // t would be of type int
     template<typename T, typename>
-      struct rebind : type_is<T>
-      {
-      };
+      struct rebind : type_is<T> {};
 
     // using meta::type_func::rebind; 
     // rebind<std::list<std::string>, int>::type t;
     // t would be of type std::list<int>
     template<template<typename> typename Tt, typename T, typename U>
-      struct rebind<Tt<T>, U> : type_is<Tt<U>>
-      {
-      };
+      struct rebind<Tt<T>, U> : type_is<Tt<U>> {};
 
     // using meta::type_func::rebind; 
     // rebind<std::pair<int, int>, std::string>::type t;
     // t would be of type std::pair<std::string, int>
     template<template<typename ...> typename Tt, typename T, typename U, typename ...Ts>
-      struct rebind<Tt<T, Ts...>, U> : type_is<Tt<U, Ts...>>
-      {
-      };
+      struct rebind<Tt<T, Ts...>, U> : type_is<Tt<U, Ts...>> {};
   }
 
   // using meta::type_func::rebind_t;
@@ -160,16 +136,12 @@ namespace meta { namespace type_func {
   // in the event of the specialization case failing and being removed
   // from the overload set
   template<typename T, typename = void>
-    struct has_type_member : ::std::false_type
-    {
-    };
+    struct has_type_member : ::std::false_type {};
 
   // Partial specialization on has_type_member matching on a successful
   // substitution of void_t<typename T::type> resolving to void
   template<typename T>
-    struct has_type_member<T, void_t<typename T::type>> : ::std::true_type
-    {
-    };
+    struct has_type_member<T, void_t<typename T::type>> : ::std::true_type {};
 
 // ---------------------------------------------------------------------------
 
@@ -179,19 +151,13 @@ namespace meta { namespace type_func {
       struct map_ts;
 
     template<template<typename> typename F>
-      struct map_ts<F> : type_is<tseq<>>
-      {
-      };
+      struct map_ts<F> : type_is<tseq<>> {};
 
     template<template<typename> typename F, typename T, typename ...Ts>
-      struct map_ts<F, T, Ts...> : prepend_tseq<typename F<T>::type, typename map_ts<F, Ts...>::type>
-      {
-      };
+      struct map_ts<F, T, Ts...> : prepend_tseq<typename F<T>::type, typename map_ts<F, Ts...>::type> {};
 
     template<template<typename> typename F, typename ...Ts>
-      struct map_ts<F, tseq<Ts...>> : map_ts<F, Ts...>
-      {
-      };
+      struct map_ts<F, tseq<Ts...>> : map_ts<F, Ts...> {};
   }
 
   template<template<typename> typename F, typename ...Ts>
@@ -204,18 +170,14 @@ namespace meta { namespace type_func {
   // IF<true, T, F>::type t
   // t is of type T
   template<bool, typename T, typename>
-    struct IF : type_is<T>
-    {
-    };
+    struct IF : type_is<T> {};
 
   // Partial specalization for matching on false
   //
   // IF<false, T, F>::type t
   // t is of type F
   template<typename T, typename F>
-    struct IF<false, T, F> : type_is<F>
-    {
-    };
+    struct IF<false, T, F> : type_is<F> {};
 
 // ---------------------------------------------------------------------------
 
@@ -230,9 +192,7 @@ namespace meta { namespace type_func {
     // Partial specialization matching on T not being in the pack at all, this
     // results in a SFINAE compatible substitution failure
     template<size_t N, typename T>
-      struct index_of<N, T>
-      {
-      };
+      struct index_of<N, T> {};
 
     // Partial specialization matching on T being the Nth element in the pack
     template<size_t N, typename T, typename ...Ts>
@@ -243,16 +203,12 @@ namespace meta { namespace type_func {
 
     // Partial specialization matching on T not being the head of the pack
     template<size_t N, typename T, typename T0, typename ...Ts>
-      struct index_of<N, T, T0, Ts...> : index_of<N + 1, T, Ts...>
-      {
-      };
+      struct index_of<N, T, T0, Ts...> : index_of<N + 1, T, Ts...> {};
   }
 
   // Convenience wrapper around index_of
   template<typename T, typename ...Ts>
-    struct index_of : detail::index_of<0, T, Ts...>
-    {
-    }; 
+    struct index_of : detail::index_of<0, T, Ts...> {}; 
 }}
 
 #endif
